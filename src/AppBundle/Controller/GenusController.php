25a28e89649b5d9faf51afb2780e94a0499d7ee0
racko92 <vladimir.rackovic.92@gmail.com>
2017-09-26 20:26:01 +0200

    Adding JSON object to controller and printing it in show.html.twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: racko92
- * Date: 26.9.17.
- * Time: 19.05
- */
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GenusController extends Controller
 {
@@ -20,14 +15,35 @@ class GenusController extends Controller
     public function showAction($genusName)
     {
         $notes = [
-            'qwer',
-            'asdf',
-            'qwer'
-        ];
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+            ];
+
         return $this->render('genus/show.html.twig', [
             'name' => $genusName,
             'notes' => $notes
         ]);
 
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Jan. 1, 2017'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Jan. 1, 2017'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Jan. 1, 2017'],
+        ];
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
     }
 }
